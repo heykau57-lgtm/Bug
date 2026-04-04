f88ad5809241365640a36e9b476cb74a0d30b191
heykau57-lgtm <heykau57@gmail.com>
2026-04-05 06:51:13 +0800

    Upload folder dan fail yang dah di-unzip

---
+++ database/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  tokens: "8485728682:AAHEDcpo-BqY1bQ_8Tj6O9X5bdV1iilILWY",  // Masukin Bot token kamu
+  owners: "7877496929", // Masukin ID Telegram kamu
+  port: "443", // Masukin Port panel kamu 
+  ipvps: "178.128.124.215" // Masukin IP vps kamu atau domain panel kamu yg asalnya ( https://AiiSigma.id ) menjadi ( http://AiiSigma.id )
+};
