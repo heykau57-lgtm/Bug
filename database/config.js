21beaaa61955ec670f4bfa0ddc49abf5fa638a7d
heykau57-lgtm <heykau57@gmail.com>
2026-04-05 07:13:07 +0800

    Update ipvps to new domain URL

--- database/config.js
+++ database/config.js
@@ -2,5 +2,5 @@ module.exports = {
   tokens: "8212739649:AAH4x6vCnYg_mj4fBRKar4Sl-ZcT1NzjzmM",  // Masukin Bot token kamu
   owners: "8429302432", // Masukin ID Telegram kamu
   port: "", // Masukin Port panel kamu 
-  ipvps: "" // Masukin IP vps kamu atau domain panel kamu yg asalnya ( https://AiiSigma.id ) menjadi ( http://AiiSigma.id )
+  ipvps: "https://bug-weld.vercel.app/" // Masukin IP vps kamu atau domain panel kamu yg asalnya ( https://AiiSigma.id ) menjadi ( http://AiiSigma.id )
 };
