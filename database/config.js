57697fabac5cf248f614d90426aa76d856277304
heykau57-lgtm <heykau57@gmail.com>
2026-04-05 07:06:31 +0800

    Update config.js

--- database/config.js
+++ database/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  tokens: "8485728682:AAHEDcpo-BqY1bQ_8Tj6O9X5bdV1iilILWY",  // Masukin Bot token kamu
-  owners: "7877496929", // Masukin ID Telegram kamu
-  port: "443", // Masukin Port panel kamu 
-  ipvps: "178.128.124.215" // Masukin IP vps kamu atau domain panel kamu yg asalnya ( https://AiiSigma.id ) menjadi ( http://AiiSigma.id )
+  tokens: "8212739649:AAH4x6vCnYg_mj4fBRKar4Sl-ZcT1NzjzmM",  // Masukin Bot token kamu
+  owners: "8429302432", // Masukin ID Telegram kamu
+  port: "", // Masukin Port panel kamu 
+  ipvps: "" // Masukin IP vps kamu atau domain panel kamu yg asalnya ( https://AiiSigma.id ) menjadi ( http://AiiSigma.id )
 };
